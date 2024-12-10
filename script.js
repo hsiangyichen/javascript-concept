7d3cb763eeb1d0dc2215455ac92e88cacb89c225
hsiangyichen <hsiangyi1025@gmail.com>
2024-12-10 15:28:47 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,47 @@
+// 1.
+let x = 3 + 5;
+console.log(x);
+
+// 2.
+let firstname = "Milk";
+let lastname = "Chen";
+console.log(firstname + " " + lastname);
+
+// 3.
+let isTrue = true;
+let isFalse = false;
+console.log(isTrue || isFalse);
+console.log(isTrue && isFalse);
+
+// 4.
+console.log((3 + 5) * 11);
+
+// 5.
+console.log(3 < 5);
+
+// 6.
+console.log(3 > 5);
+
+// 7.
+console.log(5 <= 6);
+
+// 8.
+console.log(2 == "2");
+console.log(2 === "2");
+
+// 9.
+console.log(10 % 3);
+
+// 10.
+console.log(typeof 10);
+console.log(typeof (10 % 3));
+console.log(typeof true);
+console.log(typeof null);
+console.log(typeof undefined);
+
+// 11.
+function addNumber(num1, num2) {
+  console.log(num1 + num2);
+}
+
+addNumber(10, 20);
